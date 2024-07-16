6b9275ce4b3578bdf4e2fefc48c429a12830a507
Alejandro Martin <alejandro.martingil@tno.nl>
2024-07-16 12:54:08 +0200

    TST: added ad-hoc timeseries for the electrolyzer

--- tests/test_electrolyzer.py
+++ tests/test_electrolyzer.py
@@ -37,7 +37,7 @@ class TestElectrolyzer(TestCase):
             esdl_file_name="h2.esdl",
             esdl_parser=ESDLFileParser,
             profile_reader=ProfileReaderFromFile,
-            input_timeseries_file="timeseries.csv",
+            input_timeseries_file="timeseries_electrolyzer_general.csv",
         )
 
         results = solution.extract_results()
@@ -320,7 +320,7 @@ class TestElectrolyzer(TestCase):
             esdl_file_name="h2.esdl",
             esdl_parser=ESDLFileParser,
             profile_reader=ProfileReaderFromFile,
-            input_timeseries_file="timeseries.csv",
+            input_timeseries_file="timeseries_electrolyzer_general.csv",
         )
 
         results = solution.extract_results()
