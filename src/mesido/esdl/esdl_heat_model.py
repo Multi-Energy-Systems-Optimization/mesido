c72ad9e22c619533a575b126b8a1d591254c54ef
Femke Janssen <femke.janssen@tno.nl>
2024-07-24 13:38:28 +0200

    style

--- src/mesido/esdl/esdl_heat_model.py
+++ src/mesido/esdl/esdl_heat_model.py
@@ -1776,7 +1776,7 @@ class AssetToHeatComponent(_AssetToComponentBase):
         min_voltage = asset.in_ports[0].carrier.voltage
         max_current = max_power / min_voltage
         self._set_electricity_current_nominal_and_max(asset, max_current / 2.0, max_current)
-        length = asset.attributes["length"] if asset.attributes["length"] != 0.0 else 10.0,
+        length = asset.attributes["length"] if asset.attributes["length"] != 0.0 else 10.0
         res_ohm_per_m = self._cable_get_resistance(asset)
         res_ohm = res_ohm_per_m * length
 
