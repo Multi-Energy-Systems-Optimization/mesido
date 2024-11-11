fb5eb4a1ca62ad0c2f7972c1c6dcf0ae73e50b0f
kobus.vanrooyen@tno.nl <kobus.vanrooyen@tno.nl>
2024-11-11 14:23:52 +0100

    check 4

--- tests/models/source_pipe_split_sink/src/double_pipe_heat.py
+++ tests/models/source_pipe_split_sink/src/double_pipe_heat.py
@@ -21,7 +21,7 @@ class TargetDemandGoal(Goal):
         self.target_min = optimization_problem.get_timeseries("demand.target_heat_demand")
         self.target_max = optimization_problem.get_timeseries("demand.target_heat_demand")
         self.function_range = (0.0, 2e6)
-        self.function_nominal = 1e5
+        self.function_nominal = 1e6
 
     def function(self, optimization_problem, ensemble_member):
         return optimization_problem.state("demand.Heat_demand")
