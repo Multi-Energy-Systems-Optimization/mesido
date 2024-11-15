c1cece24bc7cafa0de6639063f85b67ad0a4713b
Femke Janssen <femke.janssen@tno.nl>
2024-11-15 11:56:55 +0100

    start of the creation of a post processing utils, needed for MESIDO

---
+++ post_processing/post_processing_utils.py
@@ -0,0 +1,60 @@
+import json
+import os
+from typing import Dict
+
+import numpy as np
+
+class AliasDictResults:
+    """
+    This class allows for the handling of the result data created by MESIDO.
+    In MESIDO aliasing of variables is used to reduce computational effort. This results in fewer
+    variables being saved with their own names.
+    This class takes the loaded jsons of the results and aliases to be converted to an alias dictionary again.
+    Using the get_results method the result of each original variable name can be accessed.
+    """
+    def __init__(self, results, aliases):
+        self.results = results
+        self._aliases = aliases
+
+    def get_results(self, name):
+        if name in self.results.keys():
+            return self.results[name]
+        else:
+            alias = self._aliases[name]
+            return alias[1]*self.results[alias[0]]
+
+    def get_results_as_array(self, name):
+        return np.array(self.get_results(name))
+
+def extract_data_results_alias(data_path: str) -> [AliasDictResults, Dict]:
+    raw_data = extract_data_results(data_path)
+    results_alias_dict = create_alias_dict(raw_data)
+    return results_alias_dict, raw_data
+
+def create_alias_dict(data: Dict)-> AliasDictResults:
+    """
+    Creates the alias dict from the dictionary which contains the aliases and results.
+    :param data:
+    :return:
+    """
+    return AliasDictResults(data["results"], data["aliases"])
+
+def extract_data_results(data_path: str) -> Dict:
+    """
+    Extracts the data from the json files created after a run with MESIDO
+    :param data_path: String containing the absolute path to the folder with the json files created
+     by MESIDO.
+    :return: dictionary with the data from the json files
+    """
+    file_names = ["results", "parameters", "bounds", "aliases", "solver_stats"]
+    data = {}
+    for f in file_names:
+        input_path = os.path.join(data_path, f"{f}.json")
+        try:
+            with open(input_path) as file:
+                data_file = json.load(file)
+            data[f] = data_file
+        except IOError:
+            pass
+
+    return data
