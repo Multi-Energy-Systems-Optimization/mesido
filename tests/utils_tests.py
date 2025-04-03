38cd7ee914a61a465f26c8792b86182abc654d25
kobus.vanrooyen@tno.nl <kobus.vanrooyen@tno.nl>
2025-04-03 09:29:15 +0200

    wip

--- tests/utils_tests.py
+++ tests/utils_tests.py
@@ -343,6 +343,7 @@ def heat_to_discharge_test(solution, results):
             temp_dict[ip+"_Q_out"] = results[f"{ip}.HeatOut.Q"]
             temp_dict[ip+"_Heat_in"] = results[f"{ip}.HeatIn.Heat"]
             temp_dict[ip+"_Heat_out"] = results[f"{ip}.HeatOut.Heat"]
+            temp_dict[ip+"_flow_dir"] = results[f"{ip}.__flow_direct_var"]
         for ip in pipes_remained:
             solution.parameters(0)[f"{ip}.diameter"]
             temp_dict[ip] = solution.parameters(0)[f"{ip}.diameter"]
@@ -351,6 +352,7 @@ def heat_to_discharge_test(solution, results):
             temp_dict[ip+"_Q_out"] = results[f"{ip}.HeatOut.Q"]
             temp_dict[ip+"_Heat_in"] = results[f"{ip}.HeatIn.Heat"]
             temp_dict[ip+"_Heat_out"] = results[f"{ip}.HeatOut.Heat"]
+            temp_dict[ip+"_flow_dir"] = results[f"{ip}.__flow_direct_var"]
         
         np.testing.assert_allclose(
             results[f"{p}.HeatIn.Heat"][indices],
