1647104769757ba20582cdb47f77497d52644440
kobus.vanrooyen@tno.nl <kobus.vanrooyen@tno.nl>
2025-04-03 10:10:10 +0200

    no dh optim

--- examples/pipe_diameter_sizing/src/example.py
+++ examples/pipe_diameter_sizing/src/example.py
@@ -72,7 +72,7 @@ class PipeDiameterSizingProblem(
         self.heat_network_settings["minimum_velocity"] = 0.001
         options["heat_loss_disconnected_pipe"] = True
         options["maximum_temperature_der"] = np.inf
-        self.heat_network_settings["minimize_head_losses"] = True
+        self.heat_network_settings["minimize_head_losses"] = False # kvr
         return options
 
     def pipe_classes(self, pipe):
