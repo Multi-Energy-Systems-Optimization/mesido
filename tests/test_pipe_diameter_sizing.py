af492f10fb6f4acb39722596d9b9142348126e2a
kobus.vanrooyen@tno.nl <kobus.vanrooyen@tno.nl>
2025-04-03 09:52:53 +0200

    wip

--- tests/test_pipe_diameter_sizing.py
+++ tests/test_pipe_diameter_sizing.py
@@ -110,7 +110,7 @@ class TestPipeDiameterSizingExample(TestCase):
             )
 
         for pipe in problem.energy_system_components.get("heat_pipe", []):
-            if results[f"{pipe}__hn_diameter"] <= 1e-15:
+            if results[f"{pipe}__hn_diameter"] <= 1e-10:  # kvr issue in pipeline
                 # TODO: At the moment it is so that a pipe which is not placed (diameter == 0.) can
                 # have head loss since there is an equivalent solution where simultaniously the
                 # is_disconnected variable is also true disabling the head_loss constraints.
