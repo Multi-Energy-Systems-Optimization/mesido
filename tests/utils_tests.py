562b112cecefd2a2a8f8cd187a2b3b37a7dfe678
kobus.vanrooyen@tno.nl <kobus.vanrooyen@tno.nl>
2025-04-07 16:33:17 +0200

    check specific pipe

--- tests/utils_tests.py
+++ tests/utils_tests.py
@@ -344,16 +344,23 @@ def heat_to_discharge_test(solution, results):
             temp_dict[ip+"_Heat_in"] = results[f"{ip}.HeatIn.Heat"]
             temp_dict[ip+"_Heat_out"] = results[f"{ip}.HeatOut.Heat"]
             temp_dict[ip+"_flow_dir"] = results[f"{ip}.__flow_direct_var"]
-        for ip in pipes_remained:
-            solution.parameters(0)[f"{ip}.diameter"]
-            temp_dict[ip] = solution.parameters(0)[f"{ip}.diameter"]
-            temp_dict[ip+"_hf"] = results[f"{ip}.Heat_flow"]
-            temp_dict[ip+"_Q_in"] = results[f"{ip}.HeatIn.Q"]
-            temp_dict[ip+"_Q_out"] = results[f"{ip}.HeatOut.Q"]
-            temp_dict[ip+"_Heat_in"] = results[f"{ip}.HeatIn.Heat"]
-            temp_dict[ip+"_Heat_out"] = results[f"{ip}.HeatOut.Heat"]
-            temp_dict[ip+"_flow_dir"] = results[f"{ip}.__flow_direct_var"]
-        
+        # for ip in pipes_remained:
+        #     solution.parameters(0)[f"{ip}.diameter"]
+        #     temp_dict[ip] = solution.parameters(0)[f"{ip}.diameter"]
+        #     temp_dict[ip+"_hf"] = results[f"{ip}.Heat_flow"]
+        #     temp_dict[ip+"_Q_in"] = results[f"{ip}.HeatIn.Q"]
+        #     temp_dict[ip+"_Q_out"] = results[f"{ip}.HeatOut.Q"]
+        #     temp_dict[ip+"_Heat_in"] = results[f"{ip}.HeatIn.Heat"]
+        #     temp_dict[ip+"_Heat_out"] = results[f"{ip}.HeatOut.Heat"]
+        #     temp_dict[ip+"_flow_dir"] = results[f"{ip}.__flow_direct_var"]
+
+        temp_value = results["Pipe_9a6f_ret.HeatIn.Q"]
+        np.testing.assert_allclose(
+            0.0,
+            temp_value,
+            err_msg=f"{temp_value:.03g}",
+        )
+
         np.testing.assert_allclose(
             results[f"{p}.HeatIn.Heat"][indices],
             results[f"{p}.Q"][indices] * rho * cp * temperature,
