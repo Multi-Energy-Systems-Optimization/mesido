86762909fa2b12b15953acf742b268662a892aa8
kobus.vanrooyen@tno.nl <kobus.vanrooyen@tno.nl>
2025-07-24 14:24:19 +0200

    first working verison with elec profile

--- examples/heating_and_cooling/src/run_case.py
+++ examples/heating_and_cooling/src/run_case.py
@@ -181,7 +181,7 @@ class HeatCoolingGrowWorkflow(TestCase):
 
         class MinElectcost(Goal):
 
-            priority = 1
+            priority = 2
 
             order = 1
 
@@ -273,8 +273,8 @@ class HeatCoolingGrowWorkflow(TestCase):
 
                 goals = super().goals().copy()
 
-                for ac in self.energy_system_components.get("air_water_heat_pump_elec", []):
-                    goals.append(MinElectcost(ac))
+                # for ac in self.energy_system_components.get("air_water_heat_pump_elec", []):
+                #     goals.append(MinElectcost(ac))
 
                 return goals
 
@@ -321,8 +321,7 @@ class HeatCoolingGrowWorkflow(TestCase):
             *solution.energy_system_components.get("air_water_heat_pump_elec", []),
             *solution.energy_system_components.get("low_temperature_ates", []),
         ]:
-
-            # investment+installation costs costs
+            # investment + installation costs costs
             if asset not in solution.energy_system_components["low_temperature_ates"]:
                 investment_cost = solution.esdl_assets[
                     solution.esdl_asset_name_to_id_map[f"{asset}"]
@@ -364,7 +363,6 @@ class HeatCoolingGrowWorkflow(TestCase):
                 *solution.energy_system_components.get("air_water_heat_pump", []),
                 *solution.energy_system_components.get("air_water_heat_pump_elec", []),
             ]:
-            # solution.energy_system_components["air_water_heat_pump"]:
                 factor = solution.esdl_assets[
                     solution.esdl_asset_name_to_id_map[f"{asset}"]
                 ].attributes["COP"]
@@ -377,14 +375,34 @@ class HeatCoolingGrowWorkflow(TestCase):
                     * timesteps_hr[ii - 1]
                     / factor
                 )
+
+            # below not needed due to results[f"{asset}.Pump_power"] = 0.0
+            # if asset in solution.energy_system_components.get("low_temperature_ates", []):
+            #     pump_power = 0.0
+            #     for ii in range(1, len(solution.times())):
+            #         # pump power
+            #         pump_power += price_profile.values[ii] * results[f"{asset}.Pump_power"][ii] * timesteps_hr[ii - 1] / solution.parameters(0)[f"{asset}.pump_efficiency"]
+            #     variable_operational_cost += pump_power
+
             if asset in solution.energy_system_components.get("air_water_heat_pump_elec", []):
+                price_profile = solution.get_timeseries(
+                    f"{list(solution.get_electricity_carriers().values())[0]['name']}.price_profile"
+                )
+                # elec costs: not included in the __variable_operational_cost variable yet
+                elect_cost = 0.0
                 for ii in range(1, len(solution.times())):
-                    variable_operational_cost += (
-                        0.2e-3  # euro/Wh, carrier cost if elec is used
+                    elect_cost += (
+                        price_profile.values[ii]
                         * results[f"{asset}.Heat_flow"][ii]
                         * timesteps_hr[ii - 1]
-                        / factor
+                        / solution.parameters(0)[f"{asset}.cop"]
                     )
+                pump_power = 0.0
+                for ii in range(1, len(solution.times())):
+                    # pump power
+                    pump_power += price_profile.values[ii] * results[f"{asset}.Pump_power"][ii] * timesteps_hr[ii - 1] / solution.parameters(0)[f"{asset}.pump_efficiency"]
+
+                variable_operational_cost += pump_power
 
             total_opex += (
                 variable_operational_cost * solution.parameters(0)[f"{asset}.technical_life"]
@@ -410,6 +428,7 @@ class HeatCoolingGrowWorkflow(TestCase):
                 solution.esdl_asset_name_to_id_map[f"{asset}"]
             ].attributes["costInformation"].installationCosts.value
         assert (abs(installation_cost - results[f"{asset}__installation_cost"]) < 1.0e-8)
+        total_capex += investment_cost + installation_cost
 
         assert (abs(solution.objective_value - (total_capex + total_opex) / 1.0e6) < 1.0e-8)
 
