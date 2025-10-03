1877ac41b6fb37507833e77164008bfe0f30d74c
Femke Janssen <femke.janssen@tno.nl>
2025-10-03 16:40:40 +0200

    fix python version check in setup.py

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ Operating System :: MacOS
 if sys.version_info < (3, 10):
     sys.exit(f"Sorry, Python 3.10 to 3.11 is required. You are using {sys.version_info}")
 
-if sys.version_info > (3, 11):
+if sys.version_info >= (3, 12):
     sys.exit(f"Sorry, Python 3.10 to 3.11 is required. You are using {sys.version_info}")
 
 setup(
