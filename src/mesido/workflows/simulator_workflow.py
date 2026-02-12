980858eaa551a8661992be0a0f27193b8b008109
kobus.vanrooyen@tno.nl <kobus.vanrooyen@tno.nl>
2026-02-12 12:43:28 +0100

    Resolve PR commetn & solve profile and range constraint problem

--- src/mesido/workflows/simulator_workflow.py
+++ src/mesido/workflows/simulator_workflow.py
@@ -3,7 +3,6 @@ import logging
 
 import esdl
 
-from mesido.esdl.esdl_mixin import DBAccesType
 from mesido.esdl.esdl_mixin import ESDLMixin
 from mesido.head_loss_class import HeadLossOption
 from mesido.techno_economic_mixin import TechnoEconomicMixin
@@ -354,6 +353,7 @@ class NetworkSimulatorHIGHSWeeklyTimeStep(NetworkSimulatorHIGHS):
 @main_decorator
 def main(runinfo_path, log_level):
     logger.info("Run Network Simulator")
+    from mesido.esdl.esdl_mixin import DBAccesType
 
     kwargs = {
         "write_result_db_profiles": False,
