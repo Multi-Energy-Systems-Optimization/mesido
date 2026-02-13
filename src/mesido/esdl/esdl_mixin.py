22f1ed72d8ce0a50ee0ef0b29bacc7ef14867162
Femke Janssen <femke.janssen@tno.nl>
2026-02-13 12:29:12 +0100

    clean up

--- src/mesido/esdl/esdl_mixin.py
+++ src/mesido/esdl/esdl_mixin.py
@@ -692,8 +692,6 @@ class ESDLMixin(
         None
         """
         super().read()
-        # TODO: update for ensembles, eg ensemble_size is now still 1, if io gets updated,
-        # then the size also updates automatically
         ensemble_size = 1
         self.__ensemble = None
         if self.csv_ensemble_mode:
@@ -720,10 +718,6 @@ class ESDLMixin(
             ensemble_size=ensemble_size,
             ensemble=self.__ensemble,
         )
-        for ensemble_member_index in range(ensemble_size):
-            self.io.set_parameter("GeothermalSource_fafd.Max_heat", 10e5, ensemble_member_index)
-            if ensemble_member_index == 1:
-                self.io.set_parameter("GeothermalSource_fafd.Max_heat", 2e5, ensemble_member_index)
 
     def write(self) -> None:
         """
