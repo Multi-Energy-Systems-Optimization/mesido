d0662023b87e4b1eb60ce83de0f95d3eeb7b1d3b
kobus.vanrooyen@tno.nl <kobus.vanrooyen@tno.nl>
2026-02-23 14:01:54 +0100

    wip

--- setup.py
+++ setup.py
@@ -67,18 +67,18 @@ setup(
     ],
     # tests_require=["pytest", "pytest-runner", "numpy"],  # stil
     
-    extras_require={
-        "test": [
-            "pytest",
-            "numpy",
-            # consider removing pytest-runner (deprecated/obsolete for most setups)
-        ],
-        "all": [
-            "pytest",
-            "numpy",
-            # add any other optional stacks you meant by "all"
-        ],
-    },
+    # extras_require={
+    #     "test": [
+    #         "pytest",
+    #         "numpy",
+    #         # consider removing pytest-runner (deprecated/obsolete for most setups)
+    #     ],
+    #     "all": [
+    #         "pytest",
+    #         "numpy",
+    #         # add any other optional stacks you meant by "all"
+    #     ],
+    # },
 
     include_package_data=True,
     python_requires=">=3.10,<3.12",
