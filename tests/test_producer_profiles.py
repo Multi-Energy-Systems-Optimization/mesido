e9001b074c31d47b3dde9b8edb5acdeb4c135b7a
Femke Janssen <femke.janssen@tno.nl>
2026-05-29 09:05:49 +0200

    fix test

--- tests/test_producer_profiles.py
+++ tests/test_producer_profiles.py
@@ -139,7 +139,10 @@ class TestProducerMaxProfile(TestCase):
                 )
                 np.testing.assert_array_less(heat_produced - tol, heat_production_upper_limit)
                 np.testing.assert_array_less(
-                    3, np.isclose(heat_produced, heat_production_upper_limit, atol=1e-1, rtol=1e-5)
+                    3,
+                    np.sum(
+                        np.isclose(heat_produced, heat_production_upper_limit, atol=1e-1, rtol=1e-5)
+                    ),
                 )
                 np.testing.assert_array_less(
                     np.sum(
